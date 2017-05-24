df9ce3ba5bec895287f815058838bba3e565128d
rubine413 <rubine.lee@qq.com>
2017-05-24 10:59:22 +0800

    优化菜单

--- src/views/common/menu.vue
+++ src/views/common/menu.vue
@@ -1,180 +1,11 @@
-<style lang="stylus" scoped>
-@import '../../style/global.styl'
-    
-
-// .menu-item
-//     line-height 60px
-//     text-align center
-//     color #eee
-//     cursor pointer
-//     white-space nowrap
-//     position relative
-    
-//     &:hover
-//         background #313540
-//         color #fff
-        
-//         .submenu
-//             display block
-
-.submenu
-    position absolute
-    left 60px
-    top 0
-    background #464c5b
-    z-index 1
-    display none
-    transition all .2s ease
-    box-shadow 0 3px 10px #666
-    border-radius 0 3px 3px 0
-    
-    &:before
-        content ''
-        position absolute
-        width 1px 
-        height 100%
-        background #eee
-        left 0
-        top 0
-        z-index 1
-
-    .menu-item
-        padding 0 30px
-        line-height 50px
-        
-.menu
-    display block
-    position relative
-    margin 0 
-    padding 0
-    color $menu-color
-    cursor pointer
-    user-select none
-    background $menu-bg
-    
-    a, a:hover
-        display block
-        user-select none
-        color $menu-color
-    
-    .ivu-icon
-        font-size 16px
-        text-align center
-        width 16px
-        // vertical-align -1px
-        
-    .ivu-icon + span
-        margin-left 8px
-.menu-item
-    line-height 48px
-    overflow hidden
-    white-space nowrap
-    transition all .2s ease-in-out
-    
-    a
-        display block
-        padding-left 24px
-    
-    &:hover, &:hover a
-        color #fff
-        
-    &.menu-item-active, &.menu-item-active a
-        color #fff
-        background $primary-bg
-.menu-title
-    display flex
-    align-items center    
-    background $menu-bg
-    padding-left 24px
-    padding-right 12px
-    line-height 48px
-    
-    span
-        flex 1
-        overflow hidden
-        white-space nowrap
-        
-.menu-submenu
-    
-    .menu
-        display none
-        transition all .2s ease-in-out
-        
-    .menu-item
-        padding-left 0
-        
-        a
-            padding-left 48px
-        
-.menu-expand
-    transition all .2s ease-in-out
-.menu-opened
-    background $menu-bg-opened
-    
-    .menu-title
-        color #fff
-    .menu
-        display block
-        
-    .menu-expand
-        transform rotate(180deg)
-
-
-.menu-collapsed  
-    
-    .menu-title, & > .menu-item, & > .menu-item a
-        padding 0
-        text-align center
-        display block
-        line-height 60px
-         
-        span, .menu-expand
-             display none
-        
-        .ivu-icon
-            font-size 24px
-            width 24px
-            
-    .menu-submenu
-        position relative
-        .menu
-            display none
-            position absolute
-            top 0
-            left 100%
-            z-index 1
-            box-shadow 0 3px 10px #666
-            border-radius 0 3px 3px 0
-            
-            &:before
-                content ''
-                position absolute
-                width 1px 
-                height 100%
-                background #eee
-                left 0
-                top 0
-                z-index 1
-            .menu-item a
-                padding 0 24px
-                min-width 100px
-            
-        &:hover .menu
-            display block           
-        
-        
-        
-
-</style>
-
 <template>
     <aside class="menu-wrapper">        
         <ul class="menu" :class="{ 'menu-collapsed': collapsed }" ref="menu">
             <li class="menu-submenu">
                 <div class="menu-title" title="基础组件">
-                    <Icon type="calendar"></Icon>
+                    <Icon type="clock"></Icon>
                     <span>基础组件</span>
-                    <Icon type="ios-arrow-down menu-expand"></Icon>                    
+                    <Icon type="ios-arrow-down menu-dropdown"></Icon>                    
                 </div>
                 <ul class="menu">
                     <li class="menu-item">
@@ -192,7 +23,7 @@
                 <div class="menu-title" title="导航">
                     <Icon type="ios-list-outline"></Icon>
                     <span>导航</span>
-                    <Icon type="ios-arrow-down menu-expand"></Icon> 
+                    <Icon type="ios-arrow-down menu-dropdown"></Icon> 
                 </div>
                 <ul class="menu">
                     <li class="menu-item">
@@ -210,7 +41,7 @@
             </li>
             <li class="menu-item">
                 <router-link class="menu-link" to="/basic/form">
-                    <Icon type="paper-airplane"></Icon><span>其他</span>
+                    <Icon type="pie-graph"></Icon><span>图表</span>
                 </router-link>
             </li>
         </ul>
@@ -224,19 +55,16 @@ import { on, addClass, toggleClass, hasClass, removeClass } from '../../assets/j
 
 export default {
     name: 'admin-menu',
-
     props: {
         collapsed: {
             type: Boolean,
             default: false
         }
     },
-
     mounted() {
         var menu = this.$refs.menu;
         on(menu, 'click', e => {
-            let obj = e.target || e.srcElement;
-            
+            let obj = e.target || e.srcElement;            
             while (obj !== menu) {
                 if (hasClass(obj, 'menu-title')) {
                     toggleClass(obj.parentNode, 'menu-opened');
@@ -247,14 +75,192 @@ export default {
                     activeMenu.forEach(elem => {
                         removeClass(elem, 'menu-item-active');
                     });
+                    let activeSubMenu = menu.querySelectorAll('.menu-submenu-active');
+                    activeSubMenu.forEach(elem => {
+                        removeClass(elem, 'menu-submenu-active');
+                    });
                     addClass(obj.parentNode, 'menu-item-active');
+                    let submenuEx = obj.parentNode.parentNode.parentNode;
+                    if (hasClass(submenuEx, 'menu-submenu')) {
+                        addClass(submenuEx, 'menu-submenu-active');
+                    }
                     break;
                 }
                 obj = obj.parentNode;
-                // statement
             }            
+        });
+
+        on(menu, 'mouseover', e => {
+            let obj = e.target || e.srcElement;
+            if (hasClass(menu, 'menu-collapsed')) {
+                while (obj !== menu) {
+                    if (hasClass(obj, 'menu-submenu')) {
+                        addClass(obj, 'menu-submenu-hover');
+                        break;
+                    }                
+                    obj = obj.parentNode;
+                }  
+            }          
+        });
+        on(menu, 'mouseout', e => {
+            let obj = e.target || e.srcElement;
+            if (hasClass(menu, 'menu-collapsed')) {
+                while (obj !== menu) {
+                    if (hasClass(obj, 'menu-submenu')) {
+                        removeClass(obj, 'menu-submenu-hover');
+                        break;
+                    }                
+                    obj = obj.parentNode;
+                }  
+            }          
         });
     }
 };
 </script>
+
+<style lang="stylus" scoped>
+@import '../../style/global.styl'
+        
+.menu
+    display block
+    position relative
+    margin 0 
+    padding 0
+    color $menu-font-color
+    cursor pointer
+    user-select none     
+    
+    .ivu-icon
+        font-size 16px
+        text-align center
+        width 16px
+        margin-right 8px
+        
+    .menu-submenu &
+        display none
+        transition all .2s ease-in-out
+        
+    .menu-opened &
+        background $menu-bg-opened
+        display block
+        
+    .menu-collapsed &
+        display none
+        position absolute
+        top 0
+        left 100%
+        z-index 1
+        box-shadow 0 3px 10px #666
+        border-radius 0 3px 3px 0
+        background $menu-bg
+        
+        &:before
+            content ''
+            position absolute
+            width 1px 
+            height 100%
+            background #eee
+            left 0
+            top 0
+            z-index 1
+            
+    .menu-collapsed .menu-submenu:hover &
+        display block
+        
+        
+.menu-item
+    line-height 48px
+    overflow hidden
+    white-space nowrap
+    transition all .2s ease-in-out    
+    
+    &:hover
+        color #fff
+        
+    &.menu-item-active, &.menu-item-active .menu-link, &.menu-item-active .menu-link:hover
+        color #fff
+        background $primary-bg        
+
+    .menu-submenu &
+        padding-left 0
+        
+.menu-link
+    position relative 
+    color $menu-font-color        
+    display block
+    user-select none
+    padding-left 24px
+
+    &:hover
+        color #fff
+        background #666
+        
+    .menu-submenu &
+        padding-left 48px
+        
+    .menu-collapsed .menu-submenu &
+        padding 0 24px
+        min-width 100px
+
+.menu-title
+    display flex
+    align-items center    
+    background $menu-bg
+    padding-left 24px
+    padding-right 12px
+    line-height 48px
+    
+    span
+        flex 1
+        overflow hidden
+        white-space nowrap  
+        
+        
+    .menu-opened &, .menu-submenu-active &
+        color #fff
+        
+    .menu-collapsed .menu-opened &
+        color $menu-font-color
+        
+    .menu-collapsed &:hover, .menu-collapsed .menu-submenu-hover &
+        background #666
+        
+    .menu-collapsed .menu-submenu-active &
+        color #fff
+        background $primary-bg
+        
+
+        
+.menu-dropdown
+    transition all .2s ease-in-out
+    
+    .menu-opened &
+        transform rotate(180deg)
+        
+    .menu-collapsed &
+        display none
+
+.menu-collapsed 
+    
+    .menu-title, & > .menu-item, & > .menu-item a
+        padding 0
+        text-align center
+        display block
+        line-height 60px
+        height 60px
+                 
+        span
+             display none       
+    
+    & > .menu-item, .menu-title 
+        .ivu-icon
+            font-size 24px
+            margin 0
+            width 24px
+            line-height 60px
+              
+    .menu-submenu
+        position relative
+
+</style>
 
